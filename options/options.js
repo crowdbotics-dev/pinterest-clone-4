9c28a56b28064a4c0fefb75f26525e469506d717
Crowdbotics <engineering@crowdbotics.com>
2024-03-12 19:24:41 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pinterest-clone-4",
+  url: "https://pinterest-clone-4.botics.co",
+  api: "https://pinterest-clone-4.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
